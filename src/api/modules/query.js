cb9edaaa037f74acca938f90377cd59ef0f0e8b5
August Giles <augustgiles8@gmail.com>
2018-11-20 14:46:47 -0800

    edits to lesson-3

--- src/api/modules/query.js
+++ src/api/modules/query.js
@@ -29,27 +29,51 @@ export const controllers = {
 }
 
 export const createOne = (model) => (req, res, next) => {
-
+  return controllers.createOne(model, req.body)
+    .then(result => res.json({result}))
+    .catch(e => res.send(e))
 }
 
 export const updateOne = (model) => async (req, res, next) => {
+  const docToUpdate = req.docFromId
+  const update = req.body
 
+  return controllers.updateOne(docToUpdate, update)
+    .then(result => res.json({result}))
+    .catch(e => res.send(e))
 }
 
 export const deleteOne = (model) => (req, res, next) => {
+  const docToDelete = req.docFromId
 
+  return controllers.deleteOne(docToDelete)
+    .then(return => res.json(return))
+    .catch(e => res.send(e))
 }
 
 export const getOne = (model) => (req, res, next) => {
-
+  return controllers.getOne(req.docFromId)
+    .then(doc => res.json(doc))
+    .catch(e => res.send(e))
 }
 
 export const getAll = (model) => (req, res, next) => {
-
+  return controllers.getAll(model)
+    .then(docs => res.json(docs))
+    .catch(e => res.send(e))
 }
 
 export const findByParam = (model) => (req, res, next, id) => {
-  
+  return controllers.findByParam(model, req.docFromId)
+    .then(doc => {
+      if (!doc) {
+        next(new Error('Not Found Error'))
+      } else {
+        req.docFromId = doc
+        next()
+      }
+    })
+    .catch(e => next(e))
 }
 
 
