0d148bd82f42d59e340421e0edc84f9112cf4e6a
August Giles <augustgiles8@gmail.com>
2018-11-20 14:13:24 -0800

    changes to lesson-2

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 mongoose.Promise = global.Promise
 
 export const connect = () => {
-  return mongoose.connect('mongodb://localhost/jams', {
+  return mongoose.connect('mongodb://@ds139939.mlab.com:39939/test-database', {
     useMongoClient: true
   })
 }
